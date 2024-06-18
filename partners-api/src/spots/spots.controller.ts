06b5bb420183f38c454ee0fde183536a975904f9
Mateus <mateusc.ramos@outlook.com>
2024-06-18 15:50:43 -0300

    feat: implement spot endpoints

--- partners-api/src/spots/spots.controller.ts
+++ partners-api/src/spots/spots.controller.ts
@@ -6,37 +6,49 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
 } from '@nestjs/common';
 import { SpotsService } from './spots.service';
 import { CreateSpotDto } from './dto/create-spot.dto';
 import { UpdateSpotDto } from './dto/update-spot.dto';
 
-@Controller('spots')
+@Controller('events/:eventId/spots')
 export class SpotsController {
   constructor(private readonly spotsService: SpotsService) {}
 
   @Post()
-  create(@Body() createSpotDto: CreateSpotDto) {
-    return this.spotsService.create(createSpotDto);
+  create(
+    @Param('eventId') eventId: string,
+    @Body() createSpotDto: CreateSpotDto,
+  ) {
+    return this.spotsService.create({
+      ...createSpotDto,
+      eventId,
+    });
   }
 
   @Get()
-  findAll() {
-    return this.spotsService.findAll();
+  findAll(@Param('eventId') eventId: string) {
+    return this.spotsService.findAll(eventId);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.spotsService.findOne(+id);
+  @Get(':spotId')
+  findOne(@Param('spotId') spotId: string, @Param('eventId') eventId: string) {
+    return this.spotsService.findOne(spotId, eventId);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSpotDto: UpdateSpotDto) {
-    return this.spotsService.update(+id, updateSpotDto);
+  @Patch(':spotId')
+  update(
+    @Param('spotId') spotId: string,
+    @Param('eventId') eventId: string,
+    @Body() updateSpotDto: UpdateSpotDto,
+  ) {
+    return this.spotsService.update(spotId, eventId, updateSpotDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.spotsService.remove(+id);
+  @HttpCode(204)
+  @Delete(':spotId')
+  remove(@Param('spotId') spotId: string, @Param('eventId') eventId: string) {
+    return this.spotsService.remove(spotId, eventId);
   }
 }
--- partners-api/src/spots/spots.service.ts
+++ partners-api/src/spots/spots.service.ts
@@ -1,26 +1,57 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSpotDto } from './dto/create-spot.dto';
 import { UpdateSpotDto } from './dto/update-spot.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class SpotsService {
-  create(createSpotDto: CreateSpotDto) {
-    return 'This action adds a new spot';
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createSpotDto: CreateSpotDto & { eventId: string }) {
+    const event = await this.prismaService.event.findFirst({
+      where: { id: createSpotDto.eventId },
+    });
+
+    if (!event) {
+      throw new Error('Event not found.');
+    }
+
+    return this.prismaService.spot.create({
+      data: createSpotDto,
+    });
   }
 
-  findAll() {
-    return `This action returns all spots`;
+  findAll(eventId: string) {
+    return this.prismaService.spot.findMany({
+      where: { eventId },
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} spot`;
+  findOne(spotId: string, eventId: string) {
+    return this.prismaService.spot.findUnique({
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 
-  update(id: number, updateSpotDto: UpdateSpotDto) {
-    return `This action updates a #${id} spot`;
+  update(spotId: string, eventId: string, updateSpotDto: UpdateSpotDto) {
+    return this.prismaService.spot.update({
+      data: updateSpotDto,
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} spot`;
+  remove(spotId: string, eventId: string) {
+    return this.prismaService.spot.delete({
+      where: {
+        id: spotId,
+        eventId,
+      },
+    });
   }
 }
