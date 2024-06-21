7a7bd116ecce51cfeb2c3e948a92788d73f1d979
Mateus <mateusc.ramos@outlook.com>
2024-06-21 02:47:46 -0300

    feat: add api auth guard

--- partners-api/apps/partner1/src/partner1.module.ts
+++ partners-api/apps/partner1/src/partner1.module.ts
@@ -3,15 +3,18 @@ import { PrismaModule } from '@app/core/prisma/prisma.module';
 import { SpotsModule } from './spots/spots.module';
 import { ConfigModule } from '@nestjs/config';
 import { EventsModule } from './events/events.module';
+import { AuthModule } from '@app/core/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.partner1',
+      isGlobal: true,
     }),
     PrismaModule,
     EventsModule,
     SpotsModule,
+    AuthModule,
   ],
 })
 export class Partner1Module {}
--- partners-api/apps/partner2/src/partner2.module.ts
+++ partners-api/apps/partner2/src/partner2.module.ts
@@ -3,15 +3,18 @@ import { PrismaModule } from '@app/core/prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
 import { EventosModule } from './eventos/eventos.module';
 import { LugaresModule } from './lugares/lugares.module';
+import { AuthModule } from '@app/core/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env.partner2',
+      isGlobal: true,
     }),
     PrismaModule,
     EventosModule,
     LugaresModule,
+    AuthModule,
   ],
 })
 export class Partner2Module {}
