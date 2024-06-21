55bb65cb29f068d4d2daff8c8fc042a3d6bd3c93
Mateus <mateusc.ramos@outlook.com>
2024-06-21 01:53:01 -0300

    feat: add database integration to both partners apps

--- partners-api/apps/partner1/src/partner1.module.ts
+++ partners-api/apps/partner1/src/partner1.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '@app/core/prisma/prisma.module';
-import { EventsModule } from './events/events.module';
 import { SpotsModule } from './spots/spots.module';
+import { ConfigModule } from '@nestjs/config';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [PrismaModule, EventsModule, SpotsModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env.partner1',
+    }),
+    PrismaModule,
+    EventsModule,
+    SpotsModule,
+  ],
 })
 export class Partner1Module {}
