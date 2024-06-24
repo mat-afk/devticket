950f1e39317004c365409ca346cad3abf1eb6e95
Mateus <mateusc.ramos@outlook.com>
2024-06-24 19:54:28 -0300

    feat: add CreateEvent method

--- sales-api/internal/events/infra/repository/event_repository.go
+++ sales-api/internal/events/infra/repository/event_repository.go
@@ -14,6 +14,29 @@ func NewEventRepositoryImpl(db *sql.DB) (domain.EventRepository, error) {
 	return &EventRepositoryImpl{db: db}, nil
 }
 
+func (r *EventRepositoryImpl) CreateEvent(event *domain.Event) error {
+
+	query := `
+		INSERT INTO events (id, name, location, organization, rating, date, image_url, capacity, price, partner_id)
+		VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	`
+	_, err := r.db.Exec(
+		query,
+		event.Id,
+		event.Name,
+		event.Location,
+		event.Organization,
+		event.Rating,
+		event.Date.Format("2000-01-01 00:00:00"),
+		event.ImageURL,
+		event.Capacity,
+		event.Price,
+		event.PartnerId,
+	)
+
+	return err
+}
+
 func (r *EventRepositoryImpl) ListEvents() ([]domain.Event, error) {
 	return nil, nil
 }
@@ -28,10 +51,6 @@ func (r *EventRepositoryImpl) FindSpotsByEventId(eventId string) ([]domain.Spot,
 
 func (r *EventRepositoryImpl) FindSpotByName(eventId, spotName string) (*domain.Spot, error) {
 	return nil, nil
-}
-
-func (r *EventRepositoryImpl) CreateEvent(event *domain.Event) error {
-	return nil
 }
 
 func (r *EventRepositoryImpl) CreateSpot(spot *domain.Spot) error {
